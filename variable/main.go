9ffd3fb56c2f9848dfa586340ba4c8d18241f9de
Singgih Pratama <78544446+mastama@users.noreply.github.com>
2022-11-19 12:24:23 +0700

    penambahan cara penggunaan varible

--- variable/main.go
+++ variable/main.go
@@ -6,12 +6,14 @@ func main() {
 
 	// Deklarasi varible dengan menyertakan tipe datanya
 	//U can declared variable like this:
+	// menggunakan konsep manifest typing (Ketika deklarasi, tipe data yg digunakan harus dituliskan juga)
 
 	var name string = "Dukun Donat"
 	var location string = "Jawa Barat"
 	fmt.Println(name, location)
+	fmt.Println("---------------------------------")
 
-	// deklarasi varibale dengan var
+	// deklarasi varibale dengan var | penggunaan multiple varible
 	// also can declared variable like this
 	var (
 		firstName = "Tama"
@@ -21,7 +23,12 @@ func main() {
 	fmt.Println("Hallo perkenalkan saya", firstName, lastName, "Usia saya", age)
 	fmt.Println("Hallo perkenalkan saya"+firstName+lastName+"Usia saya", age) // perhatikan perbedaanya
 
+	var hero, power, skills = "Deku", 7000, "jalan didinding"
+	fmt.Println(hero, "adalah hero yang memiliki power", power, "dan dapat", skills)
+	fmt.Println("---------------------------------")
+
 	// deklarasi varible tanpa var dengan menggunakan fungsi fmt.Printf()
+	// menggunakan konsep type inference (type data tidak perlu dituliskan)
 	merkLaptop := "Lenovo"
 	price := 150000000
 	fmt.Printf("Aku membeli laptop dengan merk %s seharga (%d) \n", merkLaptop, price)
@@ -33,4 +40,12 @@ func main() {
 	since := 2022
 
 	fmt.Println("hallo perkenalan saya", firstName, "saya adalah", pekerjaan+" di", perusahaan+" sejak", since)
+	fmt.Println("---------------------------------")
+
+	// membuat varible kosong atau underscore _ // manis
+	var _ = "Belajar Golang Fundamental"
+	_ = "ini tidak digunakan"
+	bahasa, _ := "Golang", 2019
+	fmt.Println("penggunaan _ pada pembuatan varible dikarenakan golang tidak bisa menerima jika ada varible yang unussed di", bahasa)
+
 }
